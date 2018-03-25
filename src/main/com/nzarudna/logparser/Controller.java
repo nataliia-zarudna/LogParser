067472a0601ffb79d5be3589a2a2afd8f031ad9b
nataliia-zarudna <natalija.sirobaba@gmail.com>
2018-03-25 15:52:53 +0300

    Main method refactoring

--- src/main/com/nzarudna/logparser/Controller.java
+++ src/main/com/nzarudna/logparser/Controller.java
@@ -19,58 +19,55 @@ import java.util.SortedMap;
 
 public class Controller {
 
-	private static final String HELP_TEXT = "Usage: java -jar [jar_name] [log_path] [top_requests_duration]";
+    private static final String HELP_TEXT = "Usage: java -jar [jar_name] [log_path] [top_requests_duration]";
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        if ("-h".equals(args[0])) {
+            PrinterFactory.getInstance().printHelp(HELP_TEXT);
+            return;
+        }
+
         Instant start = Instant.now();
 
-        if ("-h".equals(args[0])) {
-        	PrinterFactory.getInstance().printHelp(HELP_TEXT);
-		} else {
-			showStatistics(args);
-		}
+        String fileName = args[0];
+        int topDurationRequestsCount = Integer.parseInt(args[1]);
 
-		Instant end = Instant.now();
-        Duration duration = Duration.between(start, end);
-        PrinterFactory.getInstance().printProgramExecuteDuration(duration.toMillis());
-	}
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
 
-	private static void showStatistics(String[] args) {
-		String fileName = args[0];
-		int topDurationRequestsCount = Integer.parseInt(args[1]);
+            List<Request> requests = new ArrayList<>();
 
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new FileReader(fileName));
-
-			List<Request> requests = new ArrayList<>();
-
-			String line;
-			while((line = reader.readLine()) != null) {
+            String line;
+            while ((line = reader.readLine()) != null) {
                 Request request = LogParser.getInstance().parseLog(line);
                 requests.add(request);
-			}
+            }
 
-			StatisticsPrinter printer = PrinterFactory.getInstance();
-			LogAnalyser logAnalyser = new LogAnalyser(requests);
+            StatisticsPrinter printer = PrinterFactory.getInstance();
+            LogAnalyser logAnalyser = new LogAnalyser(requests);
 
-			NavigableMap<Double, String> requestDurationStatistics
-					= logAnalyser.getRequestDurationStatistics();
-			printer.printDurationStatistics(requestDurationStatistics, topDurationRequestsCount);
+            NavigableMap<Double, String> requestDurationStatistics
+                    = logAnalyser.getRequestDurationStatistics();
+            printer.printDurationStatistics(requestDurationStatistics, topDurationRequestsCount);
 
-			SortedMap<Integer, Integer> hourlyRequestNumberStatistics = logAnalyser.getHourlyRequestNumberStatistics();
-			printer.drawHistogram(hourlyRequestNumberStatistics);
+            SortedMap<Integer, Integer> hourlyRequestNumberStatistics = logAnalyser.getHourlyRequestNumberStatistics();
+            printer.drawHistogram(hourlyRequestNumberStatistics);
 
-		} catch (IOException | LogParserException e) {
-			e.printStackTrace();
-		} finally {
-			if (reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+        } catch (IOException | LogParserException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
+        PrinterFactory.getInstance().printProgramExecuteDuration(duration.toMillis());
+    }
 }
